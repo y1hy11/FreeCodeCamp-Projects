5acdf97ba0cbb5b46d1b327b93d87c472aecf918
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-06 20:55:44 +0100

    refactor: Update Building a Pyramid Generator.js to use variables for character, count, and rows

--- JavaScript Algorithms and Data Structures/Building a Pyramid Generator.js
+++ JavaScript Algorithms and Data Structures/Building a Pyramid Generator.js
@@ -2,21 +2,18 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-
-
-
-
-
-
+function padRow(rowNumber, rowCount) {
+  return character.repeat(rowNumber);
+}
 
 for (let i = 0; i < count; i = i + 1) {
-    rows.push(character.repeat(i + 1))
-  }
-  
-  let result = ""
-  
-  for (const row of rows) {
-    result = result + "\n" + row;
-  }
-  
-  console.log(result);
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
