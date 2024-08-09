7e09efb0b40df6de0cb13c77f9a5e20f52e539d4
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-09 14:57:06 +0100

    refactor: Implement getAverage and hasPassingGrade functions in Building a Gradebook App.js

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Gradebook App.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Gradebook App.js
@@ -1,6 +1,34 @@
 function getAverage(scores) {
-
-}
-
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+    let sum = 0;
+  
+    for (const score of scores) {
+      sum += score;
+    }
+  
+    return sum / scores.length;
+  }
+  
+  function getGrade(score) {
+    if (score === 100) {
+      return "A++";
+    } else if (score >= 90) {
+      return "A";
+    } else if (score >= 80) {
+      return "B";
+    } else if (score >= 70) {
+      return "C";
+    } else if (score >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+  
+  function hasPassingGrade(score) {
+    
+  }
+  
+  
+  console.log(hasPassingGrade(100));
+  console.log(hasPassingGrade(53));
+  console.log(hasPassingGrade(87));
