f92bf5cd3202659f40980ed99a4f65fee363bd1a
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-12 00:13:34 +0100

    chore: Update navigation functions for store, cave, and dragon fight

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
@@ -19,7 +19,12 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 function goStore() {
-    console.log("Going to store.")
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon; 
+    button3.onclick = goTown;
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
   }
 function goCave() {
     console.log("Going to cave.");
