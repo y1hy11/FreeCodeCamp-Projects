0d13fb28931e6f9c1d8d5d2ad5e5615a53991e92
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-12 00:06:06 +0100

    chore: Add navigation functions for store, cave, and dragon fight

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
@@ -15,9 +15,15 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
 function goStore() {
     console.log("Going to store.")
   }
 function goCave() {
     console.log("Going to cave.");
+  }
+function fightDragon() {
+    console.log("Fighting dragon.");
   }
