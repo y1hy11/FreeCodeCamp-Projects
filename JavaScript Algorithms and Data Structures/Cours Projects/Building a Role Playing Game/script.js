1f09d4c0d3936e497d4b531d8c8651f5b69ae67e
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-12 21:46:14 +0100

    chore: Update function

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
@@ -16,6 +16,14 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see a sign that says \"Store\".",
+  }
+];
 
 button1.onclick = goStore;
 button2.onclick = goCave;
