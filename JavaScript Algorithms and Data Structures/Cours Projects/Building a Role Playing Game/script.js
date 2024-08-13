3dbc493cef221d2e2e477585596cd32922aa907e
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-13 23:51:45 +0100

    Refactor navigation functions and add fightSlime and fightBeast functions

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
@@ -21,26 +21,26 @@ const locations = [
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
     "button functions": [goStore, goCave, fightDragon],
-    text: "You are in the town square. You see a sign that says \"Store\".",
+    text: "You are in the town square. You see a sign that says \"Store\"."
   },
   {
     name: "store",
     "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
     "button functions": [buyHealth, buyWeapon, goTown],
-    text: "You enter the store.",
+    text: "You enter the store."
   },
   {
-    name:"cave",
+    name: "cave",
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
   }
 ];
 
+// initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
-
 
 function update(location) {
   button1.innerText = location["button text"][0];
@@ -61,7 +61,7 @@ function goStore() {
 }
 
 function goCave() {
-  console.log("Going to cave.");
+  update(locations[2]);
 }
 
 function fightDragon() {
@@ -69,7 +69,12 @@ function fightDragon() {
 }
 
 function buyHealth() {
-
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  }
 }
 
 function buyWeapon() {
