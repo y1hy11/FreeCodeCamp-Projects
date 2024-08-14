e2e3aa5cc8f0f62b72ee795badc8e2e8a687a59e
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-14 16:52:21 +0100

    "Updated buyWeapon function to display inventory and handle insufficient gold case"

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
@@ -94,7 +94,9 @@ function buyWeapon() {
     let newWeapon = weapons[currentWeaponIndex].name;
     text.innerText = "You now have a " + newWeapon + ".";
     inventory.push(newWeapon);
-    text.innerText += " In your inventory you have: ";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "You do not have enough gold to buy a weapon.";
   }
 }
 
