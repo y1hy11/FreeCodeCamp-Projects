852cd8e77b630e85c541077b16c99c87166fdf81
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-14 16:30:53 +0100

    Refactor navigation functions and add buyWeapon functionality

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
@@ -87,7 +87,11 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-
+  if (gold >= 30) {
+    gold -= 30;
+    currentWeaponIndex++;
+    goldText.innerText = gold;
+  }
 }
 
 function fightSlime() {
