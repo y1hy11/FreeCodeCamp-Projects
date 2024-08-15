5bc26f80edff9d210954e2a12abb7ac51838d279
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-15 18:30:32 +0100

    Refactor buyWeapon function to handle inventory and gold constraints

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
@@ -106,6 +106,18 @@ function buyWeapon() {
   }
 }
 
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
+  }
+}
+
 function fightSlime() {
 
 }
