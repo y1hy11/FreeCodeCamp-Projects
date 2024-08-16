0defcbaad15212d69e989c8152678315cd1629f1
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-16 12:14:40 +0100

    Implemented attack function with monster and player attacks, damage calculation, and win/lose conditions

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
@@ -160,7 +160,18 @@ function goFight() {
 }
 
 function attack() {
-
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    defeatMonster();
+  }
+}
 }
 
 function dodge() {
