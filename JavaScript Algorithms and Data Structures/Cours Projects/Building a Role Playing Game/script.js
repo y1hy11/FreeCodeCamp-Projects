eeae6def5e2222f678f4c0bebb9d7ae32b4834c9
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-16 12:21:16 +0100

    Added dodge function and defeatMonster function, updated game logic

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Role Playing Game/script.js
@@ -175,5 +175,13 @@ function attack() {
 
 
 function dodge() {
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
 
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
 }
