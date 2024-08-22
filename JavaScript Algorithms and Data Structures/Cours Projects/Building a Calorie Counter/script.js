a21e517afc12250b077e8d7cf472c1faf1f8ceb6
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-22 22:10:40 +0100

    Add placeholder for input string cleaning function Introduce `cleanInputString` function to prep for input validation enhancements. This sets the foundation for improving user input handling by cleaning strings, promoting better data integrity and user experience.

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Calorie Counter/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Calorie Counter/script.js
@@ -4,3 +4,8 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+let isError = false;
+
+function cleanInputString(str) {
+ 
+}
