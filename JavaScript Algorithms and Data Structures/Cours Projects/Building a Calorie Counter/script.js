d06525bc054dc662e0daf77d94309d72700fb8e8
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-26 19:40:31 +0100

    "Added HTMLString variable and its content in addEntry function"

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Calorie Counter/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Calorie Counter/script.js
@@ -19,4 +19,8 @@ function isInvalidInput(str) {
   function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>`;
   }
