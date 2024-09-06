cd79bc0d7246ba7fc4e540dc817bef1689897232
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-09-06 23:02:15 +0100

    Added three new paragraphs to output in calculateCalories function

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Calorie Counter/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Calorie Counter/script.js
@@ -72,6 +72,9 @@ function calculateCalories(e) {
   output.innerHTML = `
   <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
   <hr/>
+  <p>${budgetCalories} Calories Budgeted</p>
+  <p>${consumedCalories} Calories Consumed</p>
+  <p>${exerciseCalories} Calories Burned</p>
     `;
 }
 
