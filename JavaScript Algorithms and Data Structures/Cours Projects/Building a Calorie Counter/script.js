a4c0858b88378ea8215ab57e94c203e6cd168b1a
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-09-06 23:27:30 +0100

    Updated script.js: renamed clearFrom to clearForm, added code to clear input values and output, and added event listener for clear button.

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Calorie Counter/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Calorie Counter/script.js
@@ -84,14 +84,18 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
-function clearFrom() {
+function clearForm() {
   const inputContainers = Array.from(document.querySelectorAll('.input-container'));
 
   for (const container of inputContainers) {
-  container.innerHTML = '';
+    container.innerHTML = '';
   }
-  
+
+  budgetNumberInput.value = '';
+  output.innerText = '';
+  output.classList.add('hide');
 }
 
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener('click', clearForm);
