39919995349f10ffff357f57c19b2a76c9aeb4dc
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-09-10 23:49:56 +0100

    Added random index calculation and return statement to getRandomComputerResult function

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Rock, Paper, Scissors Game/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Rock, Paper, Scissors Game/script.js
@@ -1,5 +1,6 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
-    
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
   }
   console.log(getRandomComputerResult());
