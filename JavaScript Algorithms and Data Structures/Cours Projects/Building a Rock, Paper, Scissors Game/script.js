a20b23fd893dcceac60942c79249860aabea2e1d
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-09-10 23:52:02 +0100

    Refactor getRandomComputerResult function and add attribution comment

--- JavaScript Algorithms and Data Structures/Cours Projects/Building a Rock, Paper, Scissors Game/script.js
+++ JavaScript Algorithms and Data Structures/Cours Projects/Building a Rock, Paper, Scissors Game/script.js
@@ -1,9 +1,15 @@
 function getRandomComputerResult() {
-    const options = ["Rock", "Paper", "Scissors"];
-    const randomIndex = Math.floor(Math.random() * options.length);
-    return options[randomIndex];
-  }
-console.log(getRandomComputerResult());
+  const options = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
+}
+
+function hasPlayerWonTheRound(player, computer) {
+  
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
 
 /*
   Big Thanks to FreeCodeCamp 
